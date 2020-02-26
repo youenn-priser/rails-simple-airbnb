820684136dfb47d6d893bae5e23722ce0c485b4a
youenn priser <youenn.priser@gmail.com>
2020-02-26 16:42:14 +0100

    Began route and actions implementation - creation form to be added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,21 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all.order(:name)
+  end
+
+  # def show
+  #   @flat = Flat.find(params[:id])
+  # end
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
 end
